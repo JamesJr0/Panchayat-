45346d2f4e236896df2e1fd642750609c2a88a46
Cinema Pranthan <130361057+CpFlicks@users.noreply.github.com>
2023-04-11 02:20:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -48,7 +48,6 @@ async def start(client, message):
             InlineKeyboardButton('🔒 Close', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_chat_action("Typing")
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
@@ -68,7 +67,6 @@ async def start(client, message):
             InlineKeyboardButton('🔒 Close', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply_chat_action("Typing")
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
