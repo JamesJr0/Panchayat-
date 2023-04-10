6515c60511ebc20c653e02df2145c7b86ff965d6
Cinema Pranthan <130361057+CpFlicks@users.noreply.github.com>
2023-04-10 21:01:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sbrkk/MsoneSG-Updated.git /MsoneSG-Updated
+  git clone https://github.com/CpFlicks/CpSearchXbot.git /CpSearchXbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MsoneSG-Updated
+  git clone $UPSTREAM_REPO /CpSearchXbot
 fi
-cd /MsoneSG-Updated
+cd /CpSearchXbot
 pip3 install -U -r requirements.txt
 echo "#AbOutMe_DK........."
 python3 bot.py
