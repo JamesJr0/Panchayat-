fd03e79967469b3445f4edb45134edc9a3e53ae6
Lalluss <123368967+Lalluss@users.noreply.github.com>
2023-04-11 11:24:56 +0530

    Fixed

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -347,9 +347,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
             except Exception as e:
                 logger.exception(e)
             f_caption = f_caption
-            buttons = [ [ InlineKeyboardButton("⚡ᴜᴘᴅᴀᴛᴇꜱ", url="https://t.me/cpflicks"),
-                          InlineKeyboardButton("ꜱʜᴀʀᴇ ⚡", url="https://t.me/share/url?url=https://t.me/cpsearchxbot") ],
-                        [ InlineKeyboardButton("🌀 ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ 🌀", url="https://t.me/dkbotxchats") ] ]
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                         [
+                          InlineKeyboardButton('⚡ᴜᴘᴅᴀᴛᴇꜱ', url="https://t.me/cpflicks""),
+                          InlineKeyboardButton(''ꜱʜᴀʀᴇ ⚡', url="https://t.me/share/url?url=https://t.me/cpsearchxbot")
+                       ],[
+                          InlineKeyboardButton("🌀 ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ 🌀", url="https://t.me/dkbotxchats")
+                         ]
+                        ]
+                    )
         if f_caption is None:
             f_caption = f"{files.file_name}"
 
