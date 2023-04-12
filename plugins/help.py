ee98c39d85e1c3b331a796919efaabe1951cd310
Cinema Pranthan <130361057+CpFlicks@users.noreply.github.com>
2023-04-12 21:09:09 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -8,7 +8,7 @@ CMD = ["/", "."]
 async def check_alive(_, message):
     await message.reply_text("<b>Bro, Check Movie Name In #Google and Try ! Then No Results Add Movie Year and Try , Again No Results ? It's Not Available In Our Database Or Movie Not Released !\n\nബ്രോ, മൂവിയുടെ പേര് മാത്രം #Google നോക്കിയിട്ട് അടിച്ചു നോക്കുക..!!\n\nഎന്നിട്ടും കിട്ടിയില്ലെങ്കിൽ പേരിന്റെ കൂടെ മൂവി ഇറങ്ങിയ വർഷം കൂടി അടിച്ചു നോക്ക് 😁\n\nഎനിയും കിട്ടിയില്ലെങ്കിൽ ആ മൂവി ഞങ്ങളുടെ ഡാറ്റാബേസിൽ ഇല്ല, അല്ലെങ്കിൽ ആ മൂവി ഇറങ്ങിയിട്ടില്ല എന്ന് മനസ്സിലാക്കുക! 🤗⚠️\n\n📌 Any Help For Movie Searching Contact Admin.</b>")
     buttons = [ [InlineKeyboardButton("🌿 Jᴏɪɴ Fᴏʀ Mᴏʀᴇ 🌿", url="t.me/CpFlicks") ] ]
-              reply_markup = InlineKeyboardMarkup(buttons)
+    reply_markup = InlineKeyboardMarkup(buttons)
 
 @Client.on_message(filters.command("ping", CMD))
 async def ping(_, message):
