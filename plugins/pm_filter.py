b756533ce09708a0fa304219935ebfbb8ba9a0d5
Cinema Pranthan <130361057+CpFlicks@users.noreply.github.com>
2023-04-13 02:30:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -33,7 +33,7 @@ async def pm_text(bot, message):
     content = message.text
     user = message.from_user.first_name
     user_id = message.from_user.id
-    if content.startswith("/help", "/start", "/getlink") or content.startswith("#help", "#start", "#getlink"): return 
+    if content.startswith("/") or content.startswith("#"): return 
     if user_id in ADMINS: return # ignore admins
     await bot.send_message(
         chat_id=LOG_CHANNEL,
