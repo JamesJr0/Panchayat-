8de70d0656ea59fd98608b6c9782df022fd5e483
Cinema Pranthan <130361057+CpFlicks@users.noreply.github.com>
2023-04-13 22:27:28 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -382,7 +382,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             return
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
-        
+        if not files_:
+            return await query.answer('No such file exist.')
         files = files_[0]
         title = files.file_name
         size = get_size(files.file_size)
