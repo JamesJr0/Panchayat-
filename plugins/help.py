8e3e89019b48305799e03df66d6c0268d1076827
Cinema Pranthan <130361057+CpFlicks@users.noreply.github.com>
2023-04-14 20:56:44 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -18,7 +18,7 @@ async def help(client, message):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await message.reply_photo(
+    await message.send_photo(
         photo=ADD_PIC,
         chat_id=message.chat.id,
         text=HELP_TXT,
