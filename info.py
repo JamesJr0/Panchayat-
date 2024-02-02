a27105f0f7475ba22ec3fdaded12ae75eef862af
Cinema Pranthan <130361057+CpFlicks@users.noreply.github.com>
2024-02-02 23:16:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '6960947964:AAH9r6Sch96KcakHiSUE0VvSQoCKbsJ
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://graph.org/file/7a35f90b06683a98f694d.jpg https://graph.org/file/2ee759b89277cdc46fad0.jpg')).split()
-UPDATE_CHANNEL = int(environ. get('UPDATE_CHANNEL', '-1001646572603'))
+UPDATE_CHANNEL = int(environ. get('UPDATE_CHANNEL', ''))
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2092454280').split()]
