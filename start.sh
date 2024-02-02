430ce66a386c199c9c3b7cc388fe8e4d224302a9
Cinema Pranthan <130361057+CpFlicks@users.noreply.github.com>
2024-02-02 20:43:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CpFlicks/CpSearchXbot.git /CpSearchXbot
+  git clone https://github.com/CpFlicks/ProSearchFather.git /ProSearchFather
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /CpSearchXbot
+  git clone $UPSTREAM_REPO /ProSearchFather
 fi
-cd /CpSearchXbot
+cd /ProSearchFather
 pip3 install -U -r requirements.txt
-echo "#AbOutMe_DK........."
+echo "Officially Created By @AbOutMe_DK For Team @ProSearchFather! Bot is Up... Checking For Errors.."
 python3 bot.py
