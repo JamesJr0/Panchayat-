7cec9d408f9faf9262239c3de49b1fd06e10a236
Fabel Biju <158803896+Abrahamozler@users.noreply.github.com>
2025-03-20 23:45:04 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -1,122 +1,46 @@
-import io
-from pyrogram import filters, Client, enums
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from database.filters_mdb import (
-    add_filter,
-    get_filters,
-    delete_filter,
-    count_filters
-)
-from database.connections_mdb import active_connection
-from utils import get_file_id, parser, split_quotes
-from info import ADMINS
+from pyrogram import Client, filters, enums
+from pyrogram.types import Message
+from plugins.get_poster import get_poster  # Assuming this is your poster fetching function
 
-@Client.on_message(filters.command(['filter', 'add']) & filters.incoming & filters.user(ADMINS))
-async def addfilter(client, message):
-    userid = message.from_user.id if message.from_user else None
-    if not userid:
-        return await message.reply("You are anonymous admin. Use /connect {message.chat.id} in PM")
-    
-    chat_type = message.chat.type
-    args = message.text.html.split(None, 1)
+# Function to handle filter creation
+@Client.on_message(filters.command('filter') & filters.incoming)
+async def give_filter(client, message):
+    await auto_filter(client, message)
 
-    if chat_type == enums.ChatType.PRIVATE:
-        grpid = await active_connection(str(userid))
-        if grpid is not None:
-            grp_id = grpid
-            try:
-                chat = await client.get_chat(grpid)
-                title = chat.title
-            except:
-                await message.reply_text("Make sure I'm present in your group!!", quote=True)
-                return
-        else:
-            await message.reply_text("I'm not connected to any groups!", quote=True)
+# Main Auto Filter Function
+async def auto_filter(client, message):
+    await advantage_spell_chok(message)
+
+# Corrected advantage_spell_chok Function
+async def advantage_spell_chok(msg):
+    # Extracting movie request text
+    if " " in msg.text:
+        mv_rqst = msg.text.split(" ", 1)[1]  # Extract movie request text
+    else:
+        await msg.reply_text("❌ No valid movie request found. Please provide a proper query.")
+        return
+
+    try:
+        reqst_gle = mv_rqst.replace(" ", "+")
+        movies = await get_poster(reqst_gle, bulk=True)
+
+        if not movies:
+            await msg.reply_text("❌ No movies found for your request.")
             return
 
-    elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-        grp_id = message.chat.id
-        title = message.chat.title
-    else:
-        return
+        # Display movie results
+        movie_list = "\n".join(movies)
+        await msg.reply_text(f"🎬 Movies Found:\n{movie_list}")
 
-    st = await client.get_chat_member(grp_id, userid)
-    if (
-        st.status != enums.ChatMemberStatus.ADMINISTRATOR
-        and st.status != enums.ChatMemberStatus.OWNER
-        and str(userid) not in ADMINS
-    ):
-        return
+    except Exception as e:
+        await msg.reply_text(f"❌ Error occurred: {str(e)}")
 
-    if len(args) < 2:
-        await message.reply_text("Command Incomplete :(", quote=True)
-        return
-
-    extracted = split_quotes(args[1])
-    text = extracted[0].lower()
-
-    if not message.reply_to_message and len(extracted) < 2:
-        await message.reply_text("Add some content to save your filter!", quote=True)
-        return
-
-    if len(extracted) >= 2 and not message.reply_to_message:
-        reply_text, btn, alert = parser(extracted[1], text)
-        fileid = None
-        if not reply_text:
-            await message.reply_text("You cannot have buttons alone, give some text to go with it!", quote=True)
-            return
-
-    elif message.reply_to_message and message.reply_to_message.reply_markup:
-        try:
-            rm = message.reply_to_message.reply_markup
-            btn = rm.inline_keyboard
-            msg = get_file_id(message.reply_to_message)
-            if msg:
-                fileid = msg.file_id
-                reply_text = message.reply_to_message.caption.html
-            else:
-                reply_text = message.reply_to_message.text.html
-                fileid = None
-            alert = None
-        except:
-            reply_text = ""
-            btn = "[]"
-            fileid = None
-            alert = None
-
-    elif message.reply_to_message and message.reply_to_message.media:
-        try:
-            msg = get_file_id(message.reply_to_message)
-            fileid = msg.file_id if msg else None
-            reply_text, btn, alert = parser(extracted[1], text) if message.reply_to_message.sticker else parser(message.reply_to_message.caption.html, text)
-        except:
-            reply_text = ""
-            btn = "[]"
-            alert = None
-    elif message.reply_to_message and message.reply_to_message.text:
-        try:
-            fileid = None
-            reply_text, btn, alert = parser(message.reply_to_message.text.html, text)
-        except:
-            reply_text = ""
-            btn = "[]"
-            alert = None
-    else:
-        return
-
-    await add_filter(grp_id, text, reply_text, btn, fileid, alert)
-
-    await message.reply_text(
-        f"Filter for  `{text}`  added in  **{title}**",
-        quote=True,
-        parse_mode=enums.ParseMode.MARKDOWN
-    )
-
-@Client.on_message(filters.command('del') & filters.incoming & filters.user(ADMINS))
-async def deletefilter(client, message):
+# Function to delete filters
+@Client.on_message(filters.command('del') & filters.incoming)
+async def delete_filter_handler(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
-        return await message.reply("You are anonymous admin. Use /connect {message.chat.id} in PM")
+        return await message.reply("❌ You are an anonymous admin. Use /connect in PM.")
     
     chat_type = message.chat.type
 
@@ -128,10 +52,10 @@ async def deletefilter(client, message):
                 chat = await client.get_chat(grpid)
                 title = chat.title
             except:
-                await message.reply_text("Make sure I'm present in your group!!", quote=True)
+                await message.reply_text("❌ Make sure I'm present in your group!", quote=True)
                 return
         else:
-            await message.reply_text("I'm not connected to any groups!", quote=True)
+            await message.reply_text("❌ I'm not connected to any groups!", quote=True)
             return
 
     elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
@@ -152,21 +76,23 @@ async def deletefilter(client, message):
         cmd, text = message.text.split(" ", 1)
     except:
         await message.reply_text(
-            "<i>Mention the filtername which you wanna delete!</i>\n\n"
+            "<i>Mention the filter name you want to delete!</i>\n\n"
             "<code>/del filtername</code>\n\n"
-            "Use /viewfilters to view all available filters",
+            "Use /viewfilters to view all available filters.",
             quote=True
         )
         return
 
     query = text.lower()
+
     await delete_filter(message, query, grp_id)
 
+# Function to delete all filters
 @Client.on_message(filters.command('delall') & filters.incoming)
 async def delallconfirm(client, message):
     userid = message.from_user.id if message.from_user else None
     if not userid:
-        return await message.reply("You are anonymous admin. Use /connect {message.chat.id} in PM")
+        return await message.reply(f"❌ You are an anonymous admin. Use /connect {message.chat.id} in PM.")
     
     chat_type = message.chat.type
 
@@ -178,10 +104,10 @@ async def delallconfirm(client, message):
                 chat = await client.get_chat(grpid)
                 title = chat.title
             except:
-                await message.reply_text("Make sure I'm present in your group!!", quote=True)
+                await message.reply_text("❌ Make sure I'm present in your group!", quote=True)
                 return
         else:
-            await message.reply_text("I'm not connected to any groups!", quote=True)
+            await message.reply_text("❌ I'm not connected to any groups!", quote=True)
             return
 
     elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
@@ -193,31 +119,10 @@ async def delallconfirm(client, message):
     st = await client.get_chat_member(grp_id, userid)
     if st.status == enums.ChatMemberStatus.OWNER or str(userid) in ADMINS:
         await message.reply_text(
-            f"This will delete all filters from '{title}'.\nDo you want to continue??",
+            f"This will delete all filters from '{title}'.\nDo you want to continue?",
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton(text="YES", callback_data="delallconfirm")],
                 [InlineKeyboardButton(text="CANCEL", callback_data="delallcancel")]
             ]),
             quote=True
         )
-
-# Fix for `mv_rqst` error
-async def advantage_spell_chok(msg):
-    mv_rqst = msg.text.split(" ", 1)[1] if " " in msg.text else None
-
-    if not mv_rqst:
-        await msg.reply_text("❌ No valid movie request found.")
-        return
-
-    try:
-        reqst_gle = mv_rqst.replace(" ", "+")
-        movies = await get_poster(mv_rqst, bulk=True)
-
-        if not movies:
-            await msg.reply_text("❌ No movies found for your request.")
-            return
-
-        await msg.reply_text(f"🎬 Movies Found:\n{movies}")
-
-    except Exception as e:
-        await msg.reply_text(f"❌ Error occurred: {str(e)}")
